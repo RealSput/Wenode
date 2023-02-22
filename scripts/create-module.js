1b04f723dfd876315405a14671fd5e5dc903db7b
RealSput (totally sputnix) (rea <117580169+RealSput@users.noreply.github.com>
2023-02-21 19:01:25 -0600

    Create create-module.js

---
+++ scripts/create-module.js
@@ -0,0 +1,35 @@
+const path = require('path');
+const zlib = require('zlib');
+const fs = require('fs');
+let loop = (dirname) => {
+  let dirtree = {};
+  let dir = fs.readdirSync(dirname);
+  dir.forEach((x) => {
+    let joined = path.join(dirname, x);
+    let isF = fs.statSync(joined).isFile();
+    if (isF) {
+      dirtree[x] = {
+        file: {
+          contents: fs.readFileSync(joined).toString(),
+        },
+      };
+    } else {
+      dirtree[x] = {
+        directory: loop(joined),
+      };
+    }
+  });
+
+  return dirtree;
+};
+
+fs.writeFileSync(
+  process.argv[2],
+  zlib.gzipSync(
+    JSON.stringify({
+      node_modules: {
+        directory: loop('node_modules'),
+      },
+    })
+  )
+);
